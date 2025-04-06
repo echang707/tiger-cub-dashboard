dd3251bb32110c6aa6ed888acdec2e87b03e0079
Eric Chang <ericchang@Erics-MacBook-Air-4.local>
2025-04-05 22:19:39 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind is finally working! 🐯
+      </h1>
+    </div>
+  );
+}
